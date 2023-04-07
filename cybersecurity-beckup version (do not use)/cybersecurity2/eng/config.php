0590247a61389f816f1a7683181affe82d45187d
latranhaidang <s3836605@rmit.edu.vn>
2023-04-07 14:47:41 +0700

    upload cybersecurity backup version folder, just for a backup dont use this one

---
+++ cybersecurity-beckup version (do not use)/cybersecurity2/eng/config.php
@@ -0,0 +1,5 @@
+
+<?php
+    $conn = mysqli_connect("localhost","zwromzds_haidang","Haidang86.","zwromzds_cyber_db_2");
+    mysqli_set_charset($conn, "utf8");
+?>
--- cybersecurity4/eng/config.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-    $conn = mysqli_connect("localhost","coder374_haidang","Haidang86.","coder374_cyber_db_4");
-    mysqli_set_charset($conn, "utf8");
-?>
