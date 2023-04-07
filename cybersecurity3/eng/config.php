5170b48246e487d6b80b3cf0a31c3554e605b642
latranhaidang <s3836605@rmit.edu.vn>
2023-04-07 14:25:58 +0700

    upload cybersecurity3 folder

---
+++ cybersecurity3/eng/config.php
@@ -0,0 +1,5 @@
+
+<?php
+    $conn = mysqli_connect("localhost","coder374_haidang","Haidang86.","coder374_cyber_db_3");
+    mysqli_set_charset($conn, "utf8");
+?>
---
+++ cybersecurity3/vn/config.php
@@ -0,0 +1,4 @@
+<?php
+    $conn = mysqli_connect("localhost","coder374_haidang","Haidang86.","coder374_cyber_db_3");
+    mysqli_set_charset($conn, "utf8");
+?>
